61f51320734e846c220b31057f73edb87710da55
oblique <psyberbits@gmail.com>
2019-10-27 00:16:55 +0300

    tests: Compute md5 only at EOF

--- src/tests/random_file.rs
+++ src/tests/random_file.rs
@@ -10,7 +10,8 @@ pub struct RandomFile {
     size: usize,
     read_size: usize,
     rng: SmallRng,
-    md5_ctx: md5::Context,
+    md5_ctx: Option<md5::Context>,
+    md5: Option<md5::Digest>,
 }
 
 impl RandomFile {
@@ -19,24 +20,29 @@ impl RandomFile {
             size,
             read_size: 0,
             rng: SmallRng::from_entropy(),
-            md5_ctx: md5::Context::new(),
+            md5_ctx: Some(md5::Context::new()),
+            md5: None,
         }
     }
 
-    pub fn hash(self) -> md5::Digest {
-        self.md5_ctx.compute()
+    pub fn hash(&self) -> md5::Digest {
+        self.md5.expect("md5 was not computed yet").clone()
     }
 }
 
 impl Read for RandomFile {
     fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
         if self.size == self.read_size {
+            if let Some(md5_ctx) = self.md5_ctx.take() {
+                self.md5 = Some(md5_ctx.compute());
+            }
+
             Ok(0)
         } else {
             let len = cmp::min(buf.len(), self.size - self.read_size);
 
             self.rng.fill_bytes(&mut buf[..len]);
-            self.md5_ctx.consume(&buf[..len]);
+            self.md5_ctx.as_mut().unwrap().consume(&buf[..len]);
             self.read_size += len;
 
             Ok(len)
