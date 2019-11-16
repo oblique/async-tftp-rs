8828337cf39ed83f2d76e387d54ad895dd3eee67
oblique <psyberbits@gmail.com>
2019-11-16 17:08:41 +0200

    Expose server::handlers

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,11 +1,12 @@
 mod builder;
 mod handler;
-mod handlers;
 mod read_req;
 #[allow(clippy::module_inception)]
 mod server;
 #[cfg(feature = "unstable")]
 mod write_req;
+
+pub mod handlers;
 
 pub use self::builder::*;
 pub use self::handler::*;
