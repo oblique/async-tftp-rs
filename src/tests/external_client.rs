0a3d23d42dfedf512821fbf21c72df379c0a93c0
oblique <psyberbits@gmail.com>
2020-11-21 12:55:34 +0200

    Remove external client test for Windows

--- src/tests/external_client.rs
+++ src/tests/external_client.rs
@@ -1,17 +1,13 @@
 #![cfg(feature = "external-client-tests")]
-#![cfg(any(target_os = "linux", target_os = "windows"))]
-#![allow(unused_imports)]
+#![cfg(target_os = "linux")]
 
-use std::env;
 use std::fs;
 use std::io;
 use std::net::SocketAddr;
-use std::path::PathBuf;
 use std::process::Command;
 use std::process::Stdio;
 use tempfile::tempdir;
 
-#[cfg(target_os = "linux")]
 pub fn external_tftp_recv(
     filename: &str,
     server: SocketAddr,
@@ -39,38 +35,6 @@ pub fn external_tftp_recv(
         .arg(server.port().to_string())
         .status()
         .expect("atftp is not installed");
-
-    let data = fs::read(path)?;
-    Ok(md5::compute(data))
-}
-
-#[cfg(target_os = "windows")]
-pub fn external_tftp_recv(
-    filename: &str,
-    server: SocketAddr,
-    block_size: Option<u16>,
-) -> io::Result<md5::Digest> {
-    let tmp = tempdir()?;
-    let path = tmp.path().join("data");
-
-    // Expects `https://www.winagents.com/downloads/tftp.exe` is in `PATH`
-    let mut cmd = Command::new("tftp");
-
-    // Redirect output to null
-    cmd.stdin(Stdio::null()).stdout(Stdio::null()).stderr(Stdio::null());
-
-    if let Some(block_size) = block_size {
-        cmd.arg(format!("-b{}", block_size));
-    }
-
-    cmd.arg("-i")
-        .arg(format!("-p{}", server.port()))
-        .arg(server.ip().to_string())
-        .arg("get")
-        .arg(filename)
-        .arg(&path)
-        .status()
-        .expect("tftp is not installed");
 
     let data = fs::read(path)?;
     Ok(md5::compute(data))
